3f9e29ad0b41bfd47ebff46a5248f5f7dd33af43
Adelin BERARD <adberard@e3r4p15.42.fr>
2016-07-15 11:23:23 +0200

    Almost working :(

--- js/classes/Character.js
+++ js/classes/Character.js
@@ -1,44 +1,3 @@
-function Character(name, playerid)
-{
-	"use strict";
-	this.Name = name;
-	this.ctx;
-	this.movept;
-	this.posx; //coordonee sur la map
-	this.posy; //idem
-	this.PlayerId = playerid
-	this.SetPos = function(x, y)
-	{
-		this.posx = x;
-		this.posy = y;
-	}
-		// Chargement de l'image dans l'attribut image
-	this.Draw = function()
-	{
-		var sqr_x = 100;
-		var sqr_y = 100;
-		putimage(this.ctx,
-				"images/char/" + name + "/normal.gif",
-				this.posx * sqr_x - 100, //retourne la position pour etre en bas a gauche
-				this.posy * sqr_y - 100,//idem
-				100,
-				100);
-	}
-	this.Move = function(str)
-	{
-		this.ctx.clearRect(this.posx *100-100, this.posy*100-100, 100, 100);
-		if (str == 0)
-			this.posy--;
-		if (str === 1)
-			this.posx++;
-		if (str == 2)
-			this.posy++;
-		if (str === 3)
-			this.posx--;
-		this.Draw(this.ctx);
-	}
-}
-
 function Sprite(name, x , y, categorie)
 {
 	"use strict";
@@ -50,12 +9,13 @@ function Sprite(name, x , y, categorie)
 	this.y = y;
 	this.z;
 	this.direction;
-	this.current_anim = "e";
+	this.current_anim = "bad_down";
 	this.anim_speed = 100;
 	this.anim_frame;
 	this.anim_index = 0;
 	this.cat = categorie;
 	this.interval_id;
+	this.anim_isplay = true;
 	this.SetPos = function(x, y)	{
 		this.posx = x;
 		this.posy = y;
@@ -65,11 +25,14 @@ function Sprite(name, x , y, categorie)
 	{
 		that.anim_isplay == true;
 		that.interval_id = setInterval(function () {
-			if (!images[that.name + "_" + that.current_anim + "_" + that.anim_index])
-				that.anim_index = 0;
-			that.anime_frame = that.name + "_" + that.current_anim + "_" + that.anim_index;
+
+			var width = images[that.name + "_" + that.current_anim].naturalWidth
+			var nb_frame = width / 64;
+			if ( that.anim_index >= nb_frame)
+				that.anim_index = 1;
+	//		that.anim_frame = images[that.name + "_" + that.current_anim];
 			that.anim_index++;
-			that.Draw();
+			console.log("index : " + that.anim_index + "")
 		}, that.anim_speed);
 	}
 	this.Anim_stop = function()
@@ -78,9 +41,20 @@ function Sprite(name, x , y, categorie)
 		this.anim_isplay = false;
 	}
 		// Chargement de l'image dans l'attribut image
-	this.Draw = function(ctx, x, y)	{
-		if (ctx)
-			putimage(ctx, this.anime_frame, this.posx * tile_size, this.posy * tile_size, tile_size, tile_size);
+	this.Draw = function(params)	{
+		if (this.anim_isplay = true)
+		{//console.log(this.anime_frame)
+			console.log("HEEEEEY");
+			console.log(images[this.name + "_" + this.current_anim])
+			//putimage(this.ctx, this.name + "_" + this.current_anim, this.posx * tile_size, this.posy * tile_size, tile_size, tile_size);
+
+			this.ctx.drawImage(
+				images[this.name + "_" + this.current_anim],
+				this.anim_index * 64, 0,
+				64, 64,
+				this.posx * tile_size - 48, this.posy * tile_size - 48 ,
+				128, 128);
+		}
 		else
 			putimage(this.ctx, this.anime_frame, this.posx * tile_size, this.posy * tile_size, tile_size, tile_size);
 	}
@@ -96,8 +70,6 @@ function Sprite(name, x , y, categorie)
 			window.requestAnimationFrame(step);
 		  }
 		}
-
-
 	}
 	return (this);
 }
